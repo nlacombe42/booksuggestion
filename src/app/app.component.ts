c12e5d86c8b36bd2b471b5c8972d37eab2623970
Nicolas Mongrain-Lacombe <nicolas.m.lacombe@gmail.com>
2017-06-13 20:10:34 -0400

    added initial search without preferences

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,25 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Book} from "./book-suggestions/book.model";
 import {BookSuggestionsService} from "./book-suggestions/book-suggestions.service";
+import {Preference} from "./preferences/preference-display.model";
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
 	books: Book[] = [];
 
 	constructor(private bookSuggestionsService: BookSuggestionsService) {
 	}
 
-	getSuggestedBooks(preferences) {
+	ngOnInit(): void {
+		this.getSuggestedBooks([]);
+	}
+
+	getSuggestedBooks(preferences: Preference[]) {
 		this.bookSuggestionsService.getBookSuggestions(preferences)
 			.subscribe(books => {
 				this.books = books;
